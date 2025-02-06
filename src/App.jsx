c75b8a605a201bac4789f5d6ff0f0c27ac94285d
AndressaSales <andressaccs2001@gmail.com>
2025-02-06 18:21:01 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Home from './page/Home'
+import Mens from './page/Mens'
+import Womens from './page/Womens'
+import Kids from './page/Kids'
+import Card from './page/Cart'
+
+const App = () => {
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <> <Navbar/> <Home/> <Footer/> </>
+    },
+    {
+      path: '/mens',
+      element: <> <Navbar/> <Mens/> <Footer/> </>
+    },
+    {
+      path: '/women',
+      element: <> <Navbar/> <Womens/> <Footer/> </>
+    },
+    {
+      path: '/kids',
+      element: <> <Navbar/> <Kids/> <Footer/> </>
+    },
+    {
+      path: '/cart',
+      element: <> <Navbar/> <Card/> <Footer/> </>
+    }
+  ])
+
+  return (
+    <>
+      <RouterProvider  router={router}/>
+    </>
+  )
+}
+
+export default App
