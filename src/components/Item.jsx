75c5f7bdac09dba198e1b3d4d74072fdee5cc381
AndressaSales <andressaccs2001@gmail.com>
2025-02-18 15:17:30 -0300

    atualizando o component items

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,21 +1,33 @@
+import { Star, StarHalf } from 'lucide-react'
 import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Item = ({product}) => {
   return (
-    <div className='group relative'>
+    <div className='border-[2px] border-gray-300 rounded-md py-5 px-6 shadow-md hover:border-secundaryblue hover:opacity-75'>
       <Link to={`/product/${product.id}`}>
-        <div className='aspect-h-1 aspect-w-1w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80 h-96 shadow-2xl'>
-          <img src={product.image} alt={product.name} className='h-full w-full object-cover object-center' />
+        <div className='flex items-center justify-center'>
+          <img src={product.image} alt={product.name} className='w-52 h-52 object-cover object-center' />
         </div>
       </Link>
-      <div className='mt-4 flex justify-between'>
+      <div className='mt-5 flex justify-center flex-col'>
        <div>
         <Link to={`/product/${product.id}`}>
-          <span aria-hidden="true" className='inset-0 text-sm text-dark'>{product?.name}</span>
+          <h1 aria-hidden="true" className='text-lg text-dark '>{product?.name}</h1>
         </Link>
        </div>
-       <p className='text-sm font-medium text-gray-900'>R$ {product?.new_price}</p>
+     <div className='mt-4'>
+        <span className='line-through text-sm text-gray-500'>R$ {product.old_price}</span>
+        <p className='text-2xl font-medium text-gray-900'>R$ {product?.new_price} <span className='text-sm text-gray-700'>no pix</span></p>
+     </div>
+       <div className='flex mt-3'>
+        <Star color='#004643' fill='#004643' size={19} />
+        <Star color='#004643' fill='#004643' size={19} />
+        <Star color='#004643' fill='#004643' size={19} />
+        <Star color='#004643' fill='#004643' size={19} />
+        <Star color='#004643' fill='#004643' size={19} />
+        <StarHalf color='#004643' fill='#004643' size={19} />
+       </div>
       </div>
     </div>
   )
--- src/utils/new_accessories.js
+++ src/utils/new_accessories.js
@@ -7,7 +7,8 @@ let new_accessories = [
         image2: 'https://imgcentauro-a.akamaihd.net/380x380/98167204A5.jpg',
         image3: 'https://imgcentauro-a.akamaihd.net/380x380/98167204A6.jpg',
         image4: 'https://imgcentauro-a.akamaihd.net/380x380/98167204A7.jpg',
-        new_price: 99.99
+        new_price: 99.99,
+        old_price: 120
     },
     {
         id: 2,
@@ -16,7 +17,8 @@ let new_accessories = [
         image: 'https://imgcentauro-a.akamaihd.net/768x768/9835872XA6.jpg',
         image2: 'https://imgcentauro-a.akamaihd.net/768x768/9835872XA5.jpg',
         image3: 'https://imgcentauro-a.akamaihd.net/768x768/9835872XA4.jpg',
-        new_price: 99.99
+        new_price: 99.99,
+        old_price: 120
     },
     {
         id:33,
@@ -25,7 +27,8 @@ let new_accessories = [
         image: 'https://imgcentauro-a.akamaihd.net/380x380/95256101A7.jpg',
         image2: 'https://imgcentauro-a.akamaihd.net/380x380/95256101A3.jpg',
         image3: 'https://imgcentauro-a.akamaihd.net/380x380/95256101A6.jpg',
-        new_price: 40
+        new_price: 40,
+        old_price: 55
     },
     {
         id: 22,
@@ -34,7 +37,8 @@ let new_accessories = [
         image: 'https://imgcentauro-a.akamaihd.net/380x380/98809501A8.jpg',
         image2: 'https://imgcentauro-a.akamaihd.net/380x380/98809501A1.jpg',
         image3: 'https://imgcentauro-a.akamaihd.net/380x380/98809501A2.jpg',
-        new_price: 200
+        new_price: 200,
+        old_price: 315
     }
 ]
 
